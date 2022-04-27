d5f4d58aa08c05b45602d6a82c3c332b827dfd73
Mehul Soni <mehul.soni.estonia@gmail.com>
2022-04-27 14:34:00 +0300

    refactored code

--- scripts/update-price-info.js
+++ scripts/update-price-info.js
@@ -6,6 +6,33 @@ const directory_path = path.join(__dirname, '../public/tokens');
 const coingecko_base_URL = "https://api.coingecko.com/api/v3"
 const chunk_size = 10;
 
+function writeFile(sub_folders, coingeckoPriceMap) {
+  sub_folders.forEach(function (folder) {
+    const manifestJsonFilePath = path.join(directory_path, '/' + folder + "/manifest.json");
+    const manifestJsonFileRawData = fs.readFileSync(manifestJsonFilePath);
+    let manifestParsedJsonData = JSON.parse(manifestJsonFileRawData);
+    if (manifestParsedJsonData["coingeckoId"] !== null) {
+      manifestParsedJsonData["defaultPrice"] = coingeckoPriceMap[manifestParsedJsonData["coingeckoId"]]
+      let updatedManifestJsonData = JSON.stringify(manifestParsedJsonData, null, 2);
+      // re-write manifest.json
+      fs.writeFile(manifestJsonFilePath, updatedManifestJsonData, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
+function readFile(sub_folders, coingeckoPriceMap) {
+  sub_folders.forEach(function (folder) {
+    const manifestJsonFilePath = path.join(directory_path, '/' + folder + "/manifest.json");
+    const manifestJsonFileRawData = fs.readFileSync(manifestJsonFilePath);
+    let manifestParsedJsonData = JSON.parse(manifestJsonFileRawData);
+    if (manifestParsedJsonData["isSuperToken"] === true && manifestParsedJsonData["coingeckoId"] !== null) {
+      coingeckoPriceMap[manifestParsedJsonData["coingeckoId"]] = 0;
+    }
+  });
+}
+
 function modify(directory) {
   // reading token directory
   fs.readdir(directory, function (err, sub_folders) {
@@ -13,15 +40,8 @@ function modify(directory) {
     if (err) {
       return console.log('Directory not found: ' + err);
     }
-    sub_folders.forEach(function (folder) {
-      const manifestJsonFilePath = path.join(directory_path, '/' + folder + "/manifest.json");
-      const manifestJsonFileRawData = fs.readFileSync(manifestJsonFilePath);
-      let manifestParsedJsonData = JSON.parse(manifestJsonFileRawData);
-      if (manifestParsedJsonData["isSuperToken"] === true && manifestParsedJsonData["coingeckoId"] !== null) {
-        coingeckoPriceMap[manifestParsedJsonData["coingeckoId"]] = 0;
-      }
-    });
-
+    // reading manifest.json and updating coingeckoPriceMap
+    readFile(sub_folders, coingeckoPriceMap);
     // fetching coingecko prices
     https.request(`${coingecko_base_URL}/coins/markets?vs_currency=USD&ids=${Object.keys(coingeckoPriceMap).join(",")}`, function (res) {
       let data = '';
@@ -35,19 +55,7 @@ function modify(directory) {
             coingeckoPriceMap[_priceInfo.id] = _priceInfo.current_price;
         })
         // update coingecko price into manifest.json
-        sub_folders.forEach(function (folder) {
-          const manifestJsonFilePath = path.join(directory_path, '/' + folder + "/manifest.json");
-          const manifestJsonFileRawData = fs.readFileSync(manifestJsonFilePath);
-          let manifestParsedJsonData = JSON.parse(manifestJsonFileRawData);
-          if (manifestParsedJsonData["coingeckoId"] !== null) {
-            manifestParsedJsonData["defaultPrice"] = coingeckoPriceMap[manifestParsedJsonData["coingeckoId"]]
-            let updatedManifestJsonData = JSON.stringify(manifestParsedJsonData, null, 2);
-            // re-write manifest.json
-            fs.writeFile(manifestJsonFilePath, updatedManifestJsonData, (err) => {
-              if (err) throw err;
-            });
-          }
-        });
+        writeFile(sub_folders, coingeckoPriceMap);
       })
     }).end();
   });
